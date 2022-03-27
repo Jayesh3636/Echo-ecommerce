f8285527b70960765df4aeccb7b54648b9f34870
jayeshpatil <jayesh001patil@gmail.com>
2022-03-27 14:37:36 +0530

    v1.00 Added Cart design
    Added cart design and also made it available in product and wishlist page

---
+++ pages/cart/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "30vw";
+  document.getElementById("mySidepanel").style.right = "0rem";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.right = "-4rem";
+}
